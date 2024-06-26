4c3dcfa5a39d0bfe73635020a5f1ba6a70c61273
jafetegill17 <106451813+largefort@users.noreply.github.com>
2024-06-26 15:44:56 +0000

    Create successfulfetch.js

---
+++ successfulfetch.js
@@ -0,0 +1,16 @@
+var weather = 'Fetching...';
+var weatherImpact = 1.0; // Multiplier for production rate
+
+// Weather conditions and their impact
+var weatherConditions = {
+    'thunderstorm': 0.5,
+    'drizzle': 0.8,
+    'rain': 0.7,
+    'snow': 0.6,
+    'clear': 1.0,
+    'clouds': 0.9,
+    'wind': 0.8,
+    'overcast': 0.7,
+    'red weather alert': 0.3,
+    'yellow weather alert': 0.5
+};
