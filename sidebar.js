b6f88413f542a5e6847fb16ee77a6584e4ed8ba7
jafetegill17 <106451813+largefort@users.noreply.github.com>
2024-08-18 20:38:45 +0000

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,15 +1,18 @@
 function toggleSidebar() {
     const sidebar = document.getElementById('sidebar');
     const tabContents = document.querySelectorAll('.tab-content');
+    const reopenButton = document.getElementById('reopenSidebar');
 
     if (sidebar.classList.contains('hidden')) {
         sidebar.classList.remove('hidden');
+        reopenButton.style.display = 'none';
         tabContents.forEach(content => {
             content.style.left = '220px';
             content.style.width = 'calc(100% - 220px)';
         });
     } else {
         sidebar.classList.add('hidden');
+        reopenButton.style.display = 'block';
         tabContents.forEach(content => {
             content.style.left = '0';
             content.style.width = '100%';
